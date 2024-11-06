d63947729910ee2ead5be53a04142afcf9bd7f6b
MagalDevs <danielgalfla12@gmail.com>
2024-11-06 18:37:55 -0300

    react 06-11-24 2

---
+++ Explore React com JavaScript/React - Desenvolvendo com Javascript/organo01/src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <img src='/imagens/banner.png' alt='O banner principal da página do Organo'/>
+    </div>
+  )
+}
+
+export default Banner
