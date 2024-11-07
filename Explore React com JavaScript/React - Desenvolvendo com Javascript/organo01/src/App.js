c68d13e990fb6438e6fd617d29ed0ba425213f61
MagalDevs <danielgalfla12@gmail.com>
2024-11-07 10:48:46 -0300

    React 07-11-24 (pt.1)

--- Explore React com JavaScript/React - Desenvolvendo com Javascript/organo01/src/App.js
+++ Explore React com JavaScript/React - Desenvolvendo com Javascript/organo01/src/App.js
@@ -1,10 +1,11 @@
 import Banner from './components/Banner/Banner.js';
+import CampoTexto from './components/CampoTexto/CampoTexto.js';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      
+      <CampoTexto />
     </div>
   );
 }
