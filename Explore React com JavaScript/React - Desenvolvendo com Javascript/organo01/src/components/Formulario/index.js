2f931d4a01fbf336368394848d789adc522a3c6f
MagalDevs <danielgalfla12@gmail.com>
2024-11-11 13:42:22 -0300

    react 11-11-24 2

--- Explore React com JavaScript/React - Desenvolvendo com Javascript/organo01/src/components/Formulario/index.js
+++ Explore React com JavaScript/React - Desenvolvendo com Javascript/organo01/src/components/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto/CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
@@ -14,15 +15,22 @@ const Formulario = () => {
     'Mobile',
     'Inovação e Gestão'
   ]
+
+  const aoSalvar = (event) =>{
+    event.preventDefault()
+    console.log('Form foi submetido');
+    
+  }
   
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="insira a imagem" />
-        <ListaSuspensa label="Times" itens={times}/>
+        <ListaSuspensa required={true} label="Times" itens={times}/>
+        <Botao>Criar card</Botao>
       </form>
     </section>
   );
